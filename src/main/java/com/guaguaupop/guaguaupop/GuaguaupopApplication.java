04d2b7c5ee7e3fc7b21555dc0bdf6208dc323178
miribp93 <miribp93@hotmail.com>
2024-09-26 19:19:29 +0200

    mi primer commit

---
+++ src/main/java/com/guaguaupop/guaguaupop/GuaguaupopApplication.java
@@ -0,0 +1,13 @@
+package com.guaguaupop.guaguaupop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuaguaupopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuaguaupopApplication.class, args);
+	}
+
+}
